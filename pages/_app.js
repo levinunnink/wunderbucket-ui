7445c661e5086fc800eb3f7a0e231330a2df2e92
Levi Nunnink <levi@droplr.com>
2019-08-19 15:57:58 -0700

    Adds scss parsing

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,8 @@
 import React from 'react'; // eslint-disable-line
 import App from 'next/app';
 
+import '../assets/css/app.scss';
+
 class WunderbucketUIApp extends App {
   render() {
     const { Component, pageProps } = this.props;
